3fe6546876f753b2a4451f3af2ecd977d1f107d9
LimbergMG <147280485+LimbergMG@users.noreply.github.com>
2024-04-21 12:51:43 -0500

    primera parte del mapeo de objeto BD

--- src/main/java/pe/edu/cibertec/appventascibertec/model/bd/Category.java
+++ src/main/java/pe/edu/cibertec/appventascibertec/model/bd/Category.java
@@ -1,10 +1,7 @@
 package pe.edu.cibertec.appventascibertec.model.bd;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -12,6 +9,7 @@ import lombok.Data;
 @Table(name = "categories")
 public class Category {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer categoryid;
     @Column(name = "categoryname")
     private String categoryname;
