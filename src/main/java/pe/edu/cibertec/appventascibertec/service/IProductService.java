a166f94d2fa097b1d17ce27148ed4fdcf9564a43
LimbergMG <147280485+LimbergMG@users.noreply.github.com>
2024-04-22 13:26:18 -0500

    reposytory service y controller product

---
+++ src/main/java/pe/edu/cibertec/appventascibertec/service/IProductService.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.appventascibertec.service;
+
+import pe.edu.cibertec.appventascibertec.model.bd.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> listProducts();
+
+    void registerProduct(Product product);
+
+}
