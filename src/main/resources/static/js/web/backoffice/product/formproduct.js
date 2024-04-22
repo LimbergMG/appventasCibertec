cfdfc2e3d2169c32947eb9dc0b7c6c9087e17e6b
LimbergMG <147280485+LimbergMG@users.noreply.github.com>
2024-04-22 15:27:47 -0500

    product funciones javascript v2

--- src/main/resources/static/js/web/backoffice/product/formproduct.js
+++ src/main/resources/static/js/web/backoffice/product/formproduct.js
@@ -1,3 +1,13 @@
 $(document).on("click","#btnagregar", function(){
-$("#modalproduct").modal("show");
+    $("#txtnomproduct").val("");
+    $("#txtunitpriceproduct").val("");
+    $("#hddprodcod").val("0");
+    $("#modalproduct").modal("show");
+})
+
+$(document).on("click",".btnactualizar",function(){
+    $("#txtnomproduct").val($(this).attr("data-prodname"));
+    $("#txtunitpriceproduct").val($(this).attr("data-produnit"));
+    $("#hddprodcod").val($(this).attr("data-procod"));
+    $("#modalproduct").modal("show");
 })
