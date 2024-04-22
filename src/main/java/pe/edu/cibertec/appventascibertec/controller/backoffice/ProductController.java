15cc376aae923921afa39d1689d9b9bd3a5c466e
LimbergMG <147280485+LimbergMG@users.noreply.github.com>
2024-04-22 13:39:41 -0500

    controller y dto product

--- src/main/java/pe/edu/cibertec/appventascibertec/controller/backoffice/ProductController.java
+++ src/main/java/pe/edu/cibertec/appventascibertec/controller/backoffice/ProductController.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.cibertec.appventascibertec.model.bd.Product;
 import pe.edu.cibertec.appventascibertec.service.IProductService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/product")
@@ -17,5 +20,11 @@ public class ProductController {
     public String formProduct(Model model){
         model.addAttribute("listproduct", iProductService.listProducts());
         return ("backoffice/product/formproduct");
+    }
+    public List<Product> listProducts(){
+        return iProductService.listProducts();
+    }
+    public void registerProduct(){
+
     }
 }
