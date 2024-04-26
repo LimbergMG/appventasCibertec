6a67e0a436ad81103e11c63f0d577e042961046d
LimbergMG <147280485+LimbergMG@users.noreply.github.com>
2024-04-26 11:00:47 -0500

    mantenimiento usuario terminado

--- src/main/java/pe/edu/cibertec/appventascibertec/service/IUsuarioService.java
+++ src/main/java/pe/edu/cibertec/appventascibertec/service/IUsuarioService.java
@@ -10,4 +10,6 @@ public interface IUsuarioService {
     Usuario guardarUsuario(Usuario usuario);
     List<Usuario> listarUsuarios();
     Usuario obtenerUsuarioxId(int id);
+    void actualizarUsuario(Usuario usuario);
+
 }
